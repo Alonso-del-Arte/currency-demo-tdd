8db353220e242fe88c8d0ed569b91d78cdc86fac
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-01 16:24:08 -0400

    Passing revised non-existent variable test

--- src/sysops/EnvironmentVariableStore.java
+++ src/sysops/EnvironmentVariableStore.java
@@ -7,8 +7,8 @@ public class EnvironmentVariableStore {
     public static String getVariable(String label) {
         String variable = System.getenv(label);
         if (variable == null) {
-            String excMsg = "Variable for label " // + label 
-                    + " is not present on this system";
+            String excMsg = "Variable for label \"" + label 
+                    + "\" is not present on this system";
             throw new NoSuchElementException(excMsg);
         }
         return variable;
