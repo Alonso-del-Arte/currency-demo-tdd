824f47add06f98384a8336ed35eb940573a791b4
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-01 15:30:20 -0400

    Failing non-existent variable test

--- test/sysops/EnvironmentVariableStoreTest.java
+++ test/sysops/EnvironmentVariableStoreTest.java
@@ -23,4 +23,20 @@ class EnvironmentVariableStoreTest {
         }
     }
 
+    @Test
+    void testGetNonexistentVariable() {
+        String badLabel = "There should be no variable for this label";
+        Throwable exception = assertThrows(NoSuchElementException.class, () -> {
+            String badVariable = EnvironmentVariableStore.getVariable(badLabel);
+            System.out.println("Bad label \"" + badLabel
+                    + "\" should not have retrieved variable \"" + badVariable
+                    + "\"");
+        });
+        System.out.println("Trying to access variable for bad label \""
+                + badLabel + "\" correctly caused NoSuchElementException");
+        String excMsg = exception.getMessage();
+        assert excMsg != null : "Message should not be null";
+        System.out.println("\"" + excMsg + "\"");
+    }
+    
 }
