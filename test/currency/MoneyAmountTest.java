698e8fbe675416314be45576e49a16c4ec614b27
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-01 22:35:08 -0400

    Failing 0 to 9 cents test

--- test/currency/MoneyAmountTest.java
+++ test/currency/MoneyAmountTest.java
@@ -2,16 +2,26 @@ package currency;
 
 import java.util.Currency;
 import java.util.Locale;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class MoneyAmountTest {
+    
+    private static final Currency DOLLARS = Currency.getInstance(Locale.US);
+    
+    private static final Random RANDOM = new Random();
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testToStringZeroToNineCents() {
+        for (short cents = 0; cents < 9; cents++) {
+            MoneyAmount amount = new MoneyAmount(0, DOLLARS, cents);
+            String expected = "$0.0" + cents;
+            String actual = amount.toString();
+            assertEquals(expected, actual);
+        }
     }
 
 }
