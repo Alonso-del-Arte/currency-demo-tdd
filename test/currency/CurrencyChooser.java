6e60102acf19d20067fc1ee6baad3fe09968bd53
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-02 22:49:36 -0400

    Passing chooseCurrency( ) test

--- test/currency/CurrencyChooser.java
+++ test/currency/CurrencyChooser.java
@@ -1,15 +1,38 @@
 package currency;
 
+import static currency.MoneyAmountTest.RANDOM;
+
+import java.util.ArrayList;
 import java.util.Currency;
-import java.util.Locale;
-import java.util.Random;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CurrencyChooser {
     
-    // TODO: Write tests for this
+    private static final List<Currency> CURRENCIES 
+            = new ArrayList<>(Currency.getAvailableCurrencies());
+
+    private static final Set<Currency> PSEUDO_CURRENCIES = new HashSet<>();
+
+    static {
+        for (Currency currency : CURRENCIES) {
+            if (currency.getDefaultFractionDigits() < 0) {
+                PSEUDO_CURRENCIES.add(currency);
+            }
+        }
+        CURRENCIES.removeAll(PSEUDO_CURRENCIES);
+    }
+
+    /**
+     * Chooses a currency suitable for the {@link MoneyAmount} constructor.
+     * @return A currency with default fraction digits of at least 0. For 
+     * example, the Kyrgystani som (KGS), which like most world currencies by 
+     * default has two fractional digits. A som is divided into 100 tyin.
+     */
     public static Currency chooseCurrency() {
-        return Currency.getInstance(Locale.CANADA);
+        int index = RANDOM.nextInt(CURRENCIES.size());
+        return CURRENCIES.get(index);
     }
 
 }
