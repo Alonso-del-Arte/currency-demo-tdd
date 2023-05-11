c10bb93732bc7b4baf1b7c0660d6e9065af0bf65
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-10 23:30:46 -0400

    Getter stubs

--- src/currency/CurrencyMismatchException.java
+++ src/currency/CurrencyMismatchException.java
@@ -3,14 +3,27 @@ package currency;
 public class CurrencyMismatchException extends RuntimeException {
     
     private static final long serialVersionUID = 4556099108573545874L;
+    
+    private final MoneyAmount amountA, amountB;
+    
+    // TODO: Write tests for this
+    public MoneyAmount getAmountA() {
+        return this.amountB;
+    }
 
-    public CurrencyMismatchException(MoneyAmount amountA, MoneyAmount amountB) {
-        this("SORRY, DEFAULT MESSAGE NOT IMPLEMENTED YET", amountA, amountB);
+    // TODO: Write tests for this
+    public MoneyAmount getAmountB() {
+        return this.amountA;
+    }
+
+    public CurrencyMismatchException(MoneyAmount amtA, MoneyAmount amtB) {
+        this("SORRY, DEFAULT MESSAGE NOT IMPLEMENTED YET", amtA, amtB);
     }
     
-    public CurrencyMismatchException(String msg, MoneyAmount amountA, 
-            MoneyAmount amountB) {
-//        this
+    public CurrencyMismatchException(String msg, MoneyAmount amtA, 
+            MoneyAmount amtB) {
+        this.amountA = amtA;
+        this.amountB = amtB;
     }
     
 }
